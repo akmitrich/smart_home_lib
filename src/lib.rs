0aa1c4e0dd7ac7f6f839900a244149119a1e3b71
akmitrich <ak.mitrich@mail.ru>
2022-05-22 23:40:37 +0300

    Prototype 1

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+mod smart_home {
+    use std::collections::HashMap;
+    use crate::smart_room::Room;
+
+    struct Home {
+        name: String,
+        rooms: HashMap<String, Room>,
+    }
+
+    impl Home {
+        pub fn new(name: &str) -> Self {
+            Home { name, rooms: HashMap::new() }
+        }
+
+        pub fn room_list(&'a self) -> Vec<&'a Room> {
+            todo!()
+        }
+
+        pub fn add_room(&mut self, room: Room, unique_name: &str) {
+            todo!()
+        }
+
+        pub fn remove_room(&mut self, unique_name: &str) /*-> Room ?*/ {
+            todo!()
+        }
+
+        pub fn report(&self) -> String {
+            todo!()
+        }
+    }
+}
+
+mod smart_room {
+    use std::{rc::Rc, collections::HashMap};
+
+    struct Room {
+        unique_name: String,
+        devices: HashMap<String, Rc<dyn Device>>,
+    }
+
+    impl Room {
+        pub fn new(unique_name: &str) -> Self {
+            Room { unique_name, devices: HashMap::new() }
+        }
+
+        pub fn device_list(&self) -> Vec<Rc<dyn Device>> {
+            todo!()
+        }
+
+        pub fn add_device(&mut self, device: &dyn Device, unique_name: &str) {
+            todo!()
+        }
+
+        pub fn remove_device(&mut self, unique_name: &str) /* -> Rc<dyn Device> ?*/ {
+            todo!()
+        }
+    }
+
+    trait Device {
+        fn description(&self) -> String;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    
+}

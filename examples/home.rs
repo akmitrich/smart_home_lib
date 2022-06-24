14d56ec43c0f1a1c255219d80aedb7bc910d4e3c
akmitrich <ak.mitrich@mail.ru>
2022-06-24 21:26:50 +0300

    Home report (not implemented)

--- examples/home.rs
+++ examples/home.rs
@@ -2,6 +2,7 @@ use smart_home_lib::smart_home;
 use smart_home_lib::smart_device;
 fn main() {
     average_temperature_in_room();
+    report_example()
 }
 
 fn average_temperature_in_room() {
@@ -23,4 +24,20 @@ fn average_temperature_in_room() {
         let average_temperature = temperature / count as f64;
         println!("Average temperature in Main room is {}", average_temperature);
     }
+}
+
+fn report_example() {
+    let mut home = smart_home::Home::new("Home");
+    home.add_room("Main");
+    home.add_device("Main", "Thermo1",
+        smart_device::Thermometer::new(24_f64).into());
+    home.add_device("Main", "Thermo2", 
+        smart_device::Thermometer::new(26_f64).into());
+    home.add_device("Main", "S1", smart_device::Socket::new(220., 5., true).into());
+    home.add_device("Main", "S2", smart_device::Socket::new(220., 0., false).into());
+
+    home.add_room("Kitchen");
+    home.add_device("Kitchen", "Sock", smart_device::Device::new_socket());
+    home.add_device("Kitchen", "Thermo", smart_device::Device::new_thermometer());
+    println!("Generate report ... \n {}", home.report());
 }
